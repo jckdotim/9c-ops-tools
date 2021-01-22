977e8f5dc8ceb7159a08033a989d173000c498f8
JCKim <jckdotim@gmail.com>
2021-01-22 19:00:17 +0900

    Add activated function

---
+++ cli.py
@@ -0,0 +1,42 @@
+import bencodex
+import requests
+import click
+
+
+headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+}
+ADDRESS_8 = '0x0000000000000000000000000000000000000008'
+core_addr = lambda n: f'0x{n:0>40x}'
+
+def get_state(address):
+    data = {"query": f"query{{ state(address: \"{address}\") }}"}
+    response = requests.post(
+        'http://localhost:23061/graphql', headers=headers, json=data
+    )
+    try:
+        return bencodex.loads(
+            bytes.fromhex(response.json()['data']['state'])
+        )
+    except TypeError:
+        return {}
+
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.argument('address')
+def activated(address):
+    """Check given address is activated or not"""
+    activated_accs = [
+        f'0x{b.hex()}' 
+        for b 
+        in get_state(core_addr(8))['accounts']
+    ]
+    click.echo(address.lower() in activated_accs)
+
+if __name__ == '__main__':
+    cli()
